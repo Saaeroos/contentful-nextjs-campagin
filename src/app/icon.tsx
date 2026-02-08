b00df570ea9bffb1947fdd5627f935f7509c797a
saaeroos <mh.mxxm@gmail.com>
2026-02-08 14:53:14 +0100

    style: apply formatting

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -10,23 +10,21 @@ export const contentType = "image/png";
 
 export default function Icon() {
   return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 24,
-          background: "black",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          color: "#8C0014",
-          fontWeight: "bold",
-        }}
-      >
-        M
-      </div>
-    ),
+    <div
+      style={{
+        fontSize: 24,
+        background: "black",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "#8C0014",
+        fontWeight: "bold",
+      }}
+    >
+      M
+    </div>,
     {
       ...size,
     },
